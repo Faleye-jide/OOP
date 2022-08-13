01bd0de5ccf1cfcc3c05b77ef7dec83fe2de7b64
Jide Faleye <Faleyejide@gmail.com>
2022-08-13 15:42:55 +0100

    updated  python file

--- class.py
+++ class.py
@@ -1,7 +1,29 @@
+import os
+import shutil
+from datetime import datetime
+
 class Person:
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, age, name , data_list=[]) -> None:
+        self.age = age
+        self.name = name
+        self.data_list =  data_list
+    
+    def get_data(self, nums):
+        for num in nums:
+            self.data_list.append()
+        return self.data_list
+    
+
+
+# `setattr(person, 'age', 10) 
+# print(getattr(person, 'age'))
+# if hasattr(person, 'age'):
+#     print('Yes')
+# else:
+#     print('No')
     
     
-    
+# attribute = person.__dict__
+# for key, value in attribute.items():
+#     print(f'{key}: {value}')`
